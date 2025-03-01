dfb9f06e2b506aa790464b4b54a437767e4e43c3
アウン <hlumaungphyo@gmail.com>
2025-03-01 21:54:25 +0900

    Initial Commit

---
+++ BluetoothScanner/BluetoothScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BluetoothScannerApp.swift
+//  BluetoothScanner
+//
+//  Created by Hlwan Aung Phyo on 2025/03/01.
+//
+
+import SwiftUI
+
+@main
+struct BluetoothScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
